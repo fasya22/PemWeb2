e657df5d3581e4f4ae2808626bc59fe4fb652e24
Nur Halisah Fasya <69709745+fasya22@users.noreply.github.com>
2022-03-29 21:45:30 +0700

    Update navbar.php

--- util/navbar.php
+++ util/navbar.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-white p-3 px-md-3 py-md-3 fixed-top shadow sm">
         <a href="./index.php" class="navbar-brand">
             <img src="./img/logo.png" width="30" height="30" alt="" class="d-inline-block align-top">
-            FASYA
+            CAKECAINE
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler icon"></span>
