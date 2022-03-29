b2199ad8a5611dbae99caeb239857bafeff648af
Nur Halisah Fasya <69709745+fasya22@users.noreply.github.com>
2022-03-29 20:07:04 +0700

    Update connection.php

--- util/connection.php
+++ util/connection.php
@@ -3,9 +3,9 @@
 session_start();
 $host = "host = john.db.elephantsql.com";
 $port = "port = 5432";
-$dbname = "dbname = mnaejyfq";
-$username = "user = mnaejyfq";
-$password = "password = e3C1QDPaex8ClwZGSznB7P3MEYWGneza";
+$dbname = "dbname = jgdcsjkw";
+$username = "user = jgdcsjkw";
+$password = "password = lg3gGkAyxygFbNeNIpt1yoIRdsrBCFyr";
 
 $connection = pg_connect("$host $port $dbname $username $password");
 if ($connection) {
